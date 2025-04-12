8c881feace7a358f4e953269eddaac39f9046b6d
JoachimHamraoui <joachimhamraoui@gmail.com>
2025-04-12 18:27:26 +0200

    feat(quote): added quote API logic as well as custom color every new quote

--- src/App.js
+++ src/App.js
@@ -1,10 +1,50 @@
 import "./App.css";
 import Quote from "./components/Quote";
+import { useEffect, useState } from "react";
 
 function App() {
+
+  const [quote, setQuote] = useState([]);
+  const colours = [
+    "bg-pastel-pink",
+    "bg-pastel-blue",
+    "bg-pastel-green",
+    "bg-pastel-purple",
+    "bg-pastel-yellow",
+    "bg-pastel-mint",
+    "bg-pastel-peach",
+    "bg-pastel-lavender",
+  ];
+
+  const getRandomColor = () => {
+    const randomIndex = Math.floor(Math.random() * colours.length);
+    return colours[randomIndex];
+  };
+
+  const getQuote = async () => {
+    try {
+      const response = await fetch("https://api.quotable.io/quotes/random");
+      const data = await response.json();
+      const quoteObj = data[0]; // quotable returns an array
+      setQuote({ content: quoteObj.content, author: quoteObj.author });
+      console.log(quoteObj);
+      console.log(data)
+    } catch (error) {
+      console.error(error);
+    }
+    getRandomColor();
+  };
+
+  useEffect(() => {
+    getQuote();
+  }, []);
+
   return (
-    <div className="w-full h-screen bg-pink-200 flex flex-col justify-center items-center">
-      <Quote quote="Hello world !" author="Joachim" />
+    <div
+      className={`w-full h-screen ${getRandomColor()} flex flex-col justify-center items-center`}
+    >
+      <button onClick={getQuote} className="p-2 font-noto mb-4">New Quote</button>
+      <Quote quote={quote.content} author={quote.author} />
     </div>
   );
 }
--- src/components/Quote.js
+++ src/components/Quote.js
@@ -1,9 +1,9 @@
 const Quote = ({quote, author}) => {
   return (
-    <>
-      <h1 className="text-4xl font-noto italic">"{quote}"</h1>
+    <div className="w-1/2 flex flex-col">
+      <h1 className="text-4xl font-noto italic mb-4">"{quote}"</h1>
       <p className="text-2xl font-noto">- {author}</p>
-    </>
+    </div>
   );
 };
 
