052e82504ce758a653146f1140fdff41cb4a1fa1
JoachimHamraoui <joachimhamraoui@gmail.com>
2025-04-27 12:55:05 +0200

    feat(quote): cleaner code for fetching and returning

--- src/services/quoteService.js
+++ src/services/quoteService.js
@@ -1,11 +1,11 @@
 export const getQuote = async () => {
-    try {
-      const response = await fetch("https://api.quotable.io/quotes/random");
-      const data = await response.json();
-      const quoteObj = data[0]; // quotable returns an array
-      return { content: quoteObj.content, author: quoteObj.author };
-    } catch (error) {
-      console.error(error);
-    }
-  };
-
+  try {
+    const response = await fetch("https://api.quotable.io/quotes/random");
+    const data = await response.json();
+    const quoteObj = data[0];
+    return { content: quoteObj.content, author: quoteObj.author };
+  } catch (error) {
+    console.error(error);
+    return { content: "Error fetching quote.", author: "Unknown" };
+  }
+};
